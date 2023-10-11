66095480df4ade7a25c5d1af718592a51fe43076
Michael Saller <micon@techtile.media>
2023-10-11 14:53:01 +0200

    Moved to rollup

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/extension.ts',
+  output: {
+    file: 'out/main.cjs',
+    format: 'cjs', // commonJS
+  },
+  external: ['vscode'], // list external dependencies here
+  plugins: [
+    typescript({
+      tsconfig: './tsconfig.json', // your tsconfig file
+    }),
+  ],
+};
