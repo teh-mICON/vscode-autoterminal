e11cd2c4660f4a33aa693e3f1b411871e9883cb0
Michael Saller <micon@techtile.media>
2023-10-11 14:57:11 +0200

    Fixed CommonJS config

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import typescript from 'rollup-plugin-typescript2';
 export default {
   input: 'src/extension.ts',
   output: {
-    file: 'out/main.cjs',
+    file: 'out/main.js',
     format: 'cjs', // commonJS
   },
   external: ['vscode'], // list external dependencies here
